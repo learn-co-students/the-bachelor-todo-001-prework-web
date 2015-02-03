b399a7d7fde07b5053ecd79147c8d7bb8979c5e7
Katie Hoffman <ktahoffman@gmail.com>
2015-02-03 15:13:06 -0500

    update count_contestants

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -18,7 +18,15 @@ def get_contestant_name(data, occupation)
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  count = 0
+  data.each do |season, contestants|
+    contestants.each do |contestant_hash|
+      if contestant_hash["hometown"] == hometown
+        count += 1
+      end
+    end
+  end
+  count
 end
 
 def get_occupation(data, hometown)
