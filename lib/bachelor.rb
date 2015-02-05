787c3e245685dab2cff49f6e2ea24ab5b705d4e8
Katie Hoffman <ktahoffman@gmail.com>
2015-02-05 17:51:21 -0500

    remove solutions

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,49 +1,19 @@
 def get_first_name_of_season_winner(data, season)
-  data[season].each do |contestant_hash|
-    if contestant_hash["status"].downcase == "winner"
-      return contestant_hash["name"].split(" ").first
-    end
-  end
+  # code here
 end
 
 def get_contestant_name(data, occupation)
-  data.each do |season, contestants|
-    contestants.each do |contestant_hash|
-      if contestant_hash["occupation"] == occupation
-        return contestant_hash["name"]
-      end
-    end
-  end
+  # code here
 end
 
 def count_contestants_by_hometown(data, hometown)
-  count = 0
-  data.each do |season, contestants|
-    contestants.each do |contestant_hash|
-      if contestant_hash["hometown"] == hometown
-        count += 1
-      end
-    end
-  end
-  count
+  # code here
 end
 
 def get_occupation(data, hometown)
-  data.each do |season, contestants|
-    contestants.each do |contestant_hash|
-      if contestant_hash["hometown"] == hometown
-        return contestant_hash['occupation']
-      end
-    end
-  end
+  # code here
 end
 
 def get_average_age_for_season(data, season)
-  age_total = 0
-  num_of_contestants = 0
-  data[season].each do |contestant_hash|
-    age_total += (contestant_hash["age"]).to_i
-    num_of_contestants += 1
-  end
-  (age_total / num_of_contestants.to_f).round(0)
+  # code here
 end
