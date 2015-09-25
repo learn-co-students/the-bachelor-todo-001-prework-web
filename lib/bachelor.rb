f6bbf6ef0be0d71c88c9a5aae68bef4662959608
matthewmain <matthewwmain@gmail.com>
2015-09-25 12:21:13 -0400

    wrote batchelor methods

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,67 @@
-def get_first_name_of_season_winner(data, season)
-  # code here
+require 'pry'
+
+def get_first_name_of_season_winner (data, season)
+    winner = ""
+    data[season].each { | contestant_hash |
+        contestant_hash.each { | characteristic, stat |
+            if stat == "Winner"
+                winner = contestant_hash["name"]
+            end
+        }
+    }
+    winner.split[0]
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+	name = ""
+	data.each { |season, contestants_array | 
+		contestants_array.each { | contestant_hash |
+			contestant_hash.each { | characteristic, stat |
+				if stat == occupation
+					name = contestant_hash["name"]
+				end
+	        }
+	    }
+   }
+	name
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+	number_from_town = 0
+	data.each { |season, contestants_array | 
+		contestants_array.each { | contestant_hash |
+			contestant_hash.each { | characteristic, stat |
+				if stat == hometown
+					number_from_town += 1
+				end
+	        }
+	    }
+   }
+	number_from_town
 end
 
 def get_occupation(data, hometown)
-  # code here
+	occupation = ""
+	data.each { |season, contestants_array | 
+		contestants_array.each { | contestant_hash |
+			contestant_hash.each { | characteristic, stat |
+				if stat == hometown && occupation == ""
+					occupation = contestant_hash["occupation"]
+				end
+	        }
+	    }
+   }
+   occupation
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+    sum_of_ages = 0
+    number_of_contestants = 0
+    data[season].each { | contestant_hash |
+    	contestant_hash.each { | characteristic, stat |
+    		sum_of_ages += contestant_hash["age"].to_f
+    		number_of_contestants += 1
+        }
+    }
+    (sum_of_ages/number_of_contestants).round
 end
