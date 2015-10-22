a17651e7844e61b75a7e405ce60d17c223da055a
mikeappell <mikeappell@gmail.com>
2015-10-22 17:55:36 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,28 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data[season].select {|stats| stats["status"] == "Winner"}[0]["name"].split[0]
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+#   name = ""
+#   data.each {|season, people| people.each {|stats| name = stats["name"] if stats["occupation"] == occupation}}
+#   name
+  data.collect {|season, people| people.collect {|stats| stats["name"] if stats["occupation"] == occupation}}.flatten.compact[0]
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+#   count = 0
+#   data.each {|season, people| people.each {|stats| count += 1 if stats["hometown"] == hometown}}
+#   count
+  data.collect {|season, people| people.select {|stats| stats["hometown"] == hometown}}.flatten.compact.size
 end
 
 def get_occupation(data, hometown)
-  # code here
+#   occupation = ""
+#   data.each {|season, people| people.each {|stats| occupation = stats["occupation"] if stats["hometown"] == hometown && occupation == ""}}
+#   occupation
+  data.collect {|season, people| people.collect {|stats| stats["occupation"] if stats["hometown"] == hometown}}.flatten.compact[0]
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  (data[season].inject(0) {|total, stats| total += stats["age"].to_i} / data[season].size.to_f).round
 end
