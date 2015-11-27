7b33464925e1cbf8d3e43fcc8f818bfec6c39f3b
shmulim <shmuel.markel@gmail.com>
2015-11-26 21:21:28 -0800

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,5 +1,11 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+  name = nil
+  data[season].each do |key, val|
+    key.each do |key, val| key == "name" ? name = val : nil
+      return name.split[0] if key == "status" && val == "Winner"
+    end
+  end
+  name
 end
 
 def get_contestant_name(data, occupation)
