59d9f78452ce73d65963c25208f30564f0b4465f
febbraiod <donedesign1@gmail.com>
2015-12-04 15:40:01 -0500

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,5 +1,20 @@
+require 'pry'
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  first_name = ""
+  full_name = ""
+
+  data.each do |season_num, data|
+    if season_num == season
+      data.each do |i| # its in an array
+        if i.has_value?("Winner")
+          full_name = data["name"]
+        end
+      end
+    end
+  binding.pry
+  end
+
 end
 
 def get_contestant_name(data, occupation)
@@ -17,3 +32,22 @@ end
 def get_average_age_for_season(data, season)
   # code here
 end
+
+=begin
+{
+   "season 19":[
+      {
+         "name":"Ashley Iaconetti",
+         "age":"26",
+         "hometown":"Great Falls, Virginia",
+         "occupation":"Nanny/Freelance Journalist",
+         "status":""
+      },
+      {
+         "name":"Becca Tilley",
+         "age":"26",
+         "hometown":"Shreveport, Louisiana[b]",
+         "occupation":"Chiropractic Assistant",
+         "status":""
+      },
+=end
