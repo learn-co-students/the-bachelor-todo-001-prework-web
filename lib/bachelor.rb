2fb2ded34766fa3cc4dcded9b7d10685c8b3826c
febbraiod <donedesign1@gmail.com>
2015-12-04 16:30:10 -0500

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -3,51 +3,68 @@ require 'pry'
 def get_first_name_of_season_winner(data, season)
   first_name = ""
   full_name = ""
+  winnerhash = {}
 
-  data.each do |season_num, data|
-    if season_num == season
-      data.each do |i| # its in an array
-        if i.has_value?("Winner")
-          full_name = data["name"]
-        end
-      end
+  data[season].each do |i| # its in an array
+    if i.has_value?("Winner")
+      full_name = i["name"]
+      return full_name.split(" ").first
     end
-  binding.pry
   end
 
 end
 
+
 def get_contestant_name(data, occupation)
-  # code here
+
+  data.each do |season, info|
+    info.each do |i|
+      if i.has_value?(occupation)
+        return i["name"]
+      end
+    end
+  end
+
 end
+
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  
+  count = 0
+  data.each do |season, info|
+    info.each do |i|
+      if i.has_value?(hometown)
+        count += 1
+      end
+    end
+  end
+
+  count
 end
+
 
 def get_occupation(data, hometown)
-  # code here
+
+  data.each do |season, info|
+    info.each do |i|
+      if i.has_value?(hometown)
+        return i["occupation"]
+      end
+    end
+  end
+
 end
+
 
 def get_average_age_for_season(data, season)
-  # code here
-end
+  sum = 0
+  arr = []
 
-=begin
-{
-   "season 19":[
-      {
-         "name":"Ashley Iaconetti",
-         "age":"26",
-         "hometown":"Great Falls, Virginia",
-         "occupation":"Nanny/Freelance Journalist",
-         "status":""
-      },
-      {
-         "name":"Becca Tilley",
-         "age":"26",
-         "hometown":"Shreveport, Louisiana[b]",
-         "occupation":"Chiropractic Assistant",
-         "status":""
-      },
-=end
+  data[season].each do |i| # its in an array
+      arr << i["age"].to_i
+  end
+  
+  arr.each {|j| sum = sum + j}
+
+  average = (sum.to_f / arr.length.to_f).round
+end
