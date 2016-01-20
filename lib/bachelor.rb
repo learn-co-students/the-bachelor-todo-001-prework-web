792377bf1bbc81e0ec0f0b8ddce91addd9e1bf97
fossbender <fossbender@gmail.com>
2016-01-20 10:53:46 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,17 +1,8 @@
 require 'pry'
 
-# each season contains an array of hashes
-# `season` passed in is a single string season name
-# data.size = 11 (11 season hashes)
-
 def get_first_name_of_season_winner(data, season)
-
-  # (key) s_num is the season number as a String
-  # (value) c_arr is an Array (size 30 for season 19) of the season contestant hashes
   data.each do | s_num, c_arr |
     if s_num == season
-
-      # con_att is a hash of a single contestant
       c_arr.each do | con_att |
       if con_att['status'].capitalize == 'winner'.capitalize
         return con_att['name'].split.first
@@ -49,7 +40,6 @@ def get_occupation(data, hometown)
     c_arr.each do | con_att |
       if con_att['hometown'] == hometown
         return con_att['occupation']
-        binding.pry
       end
     end
   end
@@ -66,5 +56,5 @@ def get_average_age_for_season(data, season)
       end
     end
   end
-  average_age = (total_age / season_size).round.to_i
+  (total_age / season_size).round.to_i
 end
