f9a126991457933528dff092e778e35321df43ea
chintas1 <chintas1@tcnj.edu>
2016-02-03 16:36:05 -0500

    Simplifies Methods

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,49 +1,29 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
-  winner = ""
-  data[season].each do |contestant|
-    winner = (contestant["name"].split(" "))[0] if contestant["status"] == "Winner"
-  end
-  winner
+  winner = data[season].find {|contestant| contestant.fetch("status") == "Winner"}
+  winner.fetch("name").split(" ")[0]
 end
 
 def get_contestant_name(data, occupation)
-  # code here
-  contestant_w_occ = ""
-  data.each do |season, contestants|
-    contestants.each do |contestant|
-      contestant_w_occ = contestant["name"] if contestant["occupation"] == occupation
-    end
-  end
-  contestant_w_occ
+  get_contestants(data).find {|contestant| contestant.fetch("occupation") == occupation}.fetch("name")
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
-  count = 0
-  data.each do |season, contestants|
-    contestants.each do |contestant|
-      count += 1 if contestant["hometown"] == hometown
-    end
-  end
-  count
+  get_contestants(data).select {|contestant| contestant.fetch("hometown") == hometown}.size
 end
 
 def get_occupation(data, hometown)
-  # code here
-  data.each do |season, contestants|
-    contestants.each do |contestant|
-      return contestant["occupation"] if contestant["hometown"] == hometown
-    end
-  end
-  "No one is from #{hometown}."
+  contestant_occupation = get_contestants(data).find {|contestant| contestant.fetch("hometown") == hometown}.fetch("occupation")
+  contestant_occupation || "No one is from #{hometown}."
 end
 
 def get_average_age_for_season(data, season)
-  # code here
-  age_total = 0
-  data[season].each do |contestant|
-    age_total += contestant["age"].to_f
+  age_total = data[season].inject(0) do |age_total, contestant|
+    age_total + contestant["age"].to_f
   end
-  (age_total/data[season].size).round
+  (age_total/(data.fetch(season).size)).round
+end
+
+#////////////////////////////////////////////////////////Helpers
+def get_contestants(data)
+  data.values.flatten
 end
