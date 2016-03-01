e89abb1179af8161f9773115c6361cf6c947758f
Brian Bier <briansbier@gmail.com>
2016-02-29 22:27:41 -0500

    Challenge complete

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,55 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data[season].each do |array|
+    array.each do |key,value|
+      if key == "status" && value == "Winner"
+        return array["name"].scan(/\w+\s/).join("").strip
+      end
+    end
+  end
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  person = []
+    data.each do |key,value|
+      value.each do |element|
+      if element["occupation"] == occupation
+         person << element["name"]
+      end
+      end
+  end
+  person.join("")
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  count = 0
+    data.each do |key,value|
+      value.each do |element|
+      if element["hometown"] == hometown
+         count += 1
+      end
+      end
+  end
+  count
 end
 
 def get_occupation(data, hometown)
-  # code here
+    occupation = []
+    data.each do |key,value|
+      value.each do |element|
+      if element["hometown"] == hometown
+         occupation << element["occupation"]
+      end
+      end
+  end
+  occupation.first
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+    container = []
+    data[season].each do |array|
+      array.each do |key,value|
+        container << array["age"].to_i
+     end
+  end
+  (container.reduce(:+) / container.length).ceil
 end
