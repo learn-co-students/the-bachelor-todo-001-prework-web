9707d1d629956f5f8b33905d9d06b49af9950b37
bfitzpa5 <brendanjfitzpatrick0@gmail.com>
2016-03-02 00:37:08 -0500

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,59 @@
+require 'json'
+
+#bachelor = JSON.parse(File.read('spec/fixtures/contestants.json'))
+
+#this_season = "season 10"
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data[season].each do |contestants_info|
+    if contestants_info["status"] == "Winner"
+      return contestants_info["name"].split(" ")[0]
+    end
+  end
 end
 
+#get_first_name_of_season_winner(bachelor, this_season)
+
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |season, season_contestants|
+    season_contestants.each do |contestants_info|
+      if contestants_info["occupation"] == occupation
+        return contestants_info["name"]
+      end
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  counter = 0
+  data.each do |season, season_contestants|
+    season_contestants.each do |contestants_info|
+      if contestants_info["hometown"] == hometown
+        counter += 1
+      end
+    end
+  end
+  counter
 end
 
 def get_occupation(data, hometown)
-  # code here
+  hometown_array = []
+  data.each do |season, season_contestants|
+    season_contestants.each do |contestants_info|
+      if contestants_info["hometown"] == hometown
+        hometown_array << contestants_info["occupation"]
+      end
+    end
+  end
+  hometown_array[0]
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  contestant_age_total = 0
+  num_of_contestants = 0
+  data[season].each do |contestant_hash|
+    contestant_age_total += (contestant_hash["age"]).to_i
+    num_of_contestants += 1
+  end
+  (contestant_age_total / num_of_contestants.to_f).round(0)
 end
