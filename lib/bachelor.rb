7a65781fc5ad062ad84e667461d9c05325702377
jaspercurry <jaspercurry@gmail.com>
2016-05-15 16:01:08 -0400

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,67 @@
+def get_first_name_of_season_winner1(data, season)
+  data[season].each do |hash|
+    if hash["status"] == "Winner"
+      return hash["name"].split(" ")[0]
+    end
+  end
+end
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  i = 0
+  until data[season][i]["status"] == "Winner"
+    i +=1
+  end
+  data[season][i]["name"].split(" ")[0]
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |season, info|
+    info.each do |hash|
+      hash.each do |type, deets|
+        if deets == occupation
+          return hash["name"]
+        end
+      end
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+ counter = 0
+  data.each do |season, info|
+    info.each do |hash|
+      hash.each do |type, deets|
+        if deets == hometown
+          counter += 1
+        end
+      end
+    end
+  end
+  counter
 end
 
+
 def get_occupation(data, hometown)
-  # code here
+ data.each do |season, info|
+    info.each do |hash|
+      hash.each do |type, deets|
+        if deets == hometown
+          return hash["occupation"]
+        end
+      end
+    end
+  end 
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  ages = []
+    data[season].each do |hash|
+      hash.each do |type, data|
+        if type == "age"
+          ages << data.to_f
+        end
+      end
+    end
+  ages
+  (ages.reduce(:+) / ages.count).round
 end
