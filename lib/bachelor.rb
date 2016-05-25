3c7dedf1eff30203a8c4583979be4c08949aeb26
KevinDTran <kevin@zipmark.com>
2016-05-24 22:59:14 -0500

    mid stage update

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,60 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data[season].each do |x|
+    if x["status"] == "Winner"
+      return x["name"].split(" ").first
+    else
+    end
+  end
 end
 
+
+
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |seasons, contestants|
+    contestants.each do |contestant, information |
+      if contestant["occupation"] == occupation
+        return contestant["name"]
+      else
+        #pass
+      end
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  shared_home = []
+  data.each do |seasons, contestants|
+    contestants.each do |contestant, information|
+      if contestant["hometown"] == hometown
+        shared_home << contestant
+      else
+        #pass
+      end
+    end
+  end
+  return shared_home.count
 end
 
-def get_occupation(data, hometown)
-  # code here
+def get_occupation(data, count_contestants_by_hometownetown)
+  entry = count_contestants_by_hometownetown.split(",")
+  entry_name = entry.first
+  entry_location = entry.last
+  puts entry_name
+  puts entry_location
+  winner = []
+  data.each do |seasons, contestants|
+    contestants.each do |contestant, information|
+      if contestant["name"] == entry_name
+        winner << contestant["occupation"]
+      else
+      end
+    end
+  end
+  return winner
 end
 
 def get_average_age_for_season(data, season)
   # code here
 end
+
+
