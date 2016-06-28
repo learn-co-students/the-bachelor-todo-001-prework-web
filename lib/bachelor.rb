c79939f37bd5df478382f6c1ca0660159d686382
Derick Quartey <niikwartey1@gmail.com>
2016-06-28 04:17:22 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,64 @@
 def get_first_name_of_season_winner(data, season)
   # code here
+  data.each do |data_season, season_data|
+		if data_season == season
+			season_data.each do |profile|
+				if profile["status"] == "Winner"
+					return profile["name"].split[0]
+				end
+			end
+		end
+	end
 end
 
 def get_contestant_name(data, occupation)
   # code here
+  data.each do |data_season, season_data|
+		season_data.each do |profile|
+			if profile["occupation"] == occupation
+				return profile["name"]
+			end
+		end
+	end
 end
 
 def count_contestants_by_hometown(data, hometown)
   # code here
+  count = 0
+  data.each do |data_season, season_data|
+		season_data.each do |profile|
+			if profile["hometown"] == hometown
+			  count += 1
+			end
+		end
+	end
+  count
 end
 
 def get_occupation(data, hometown)
   # code here
+  data.each do |data_season, season_data|
+		season_data.each do |profile|
+			if profile["hometown"] == hometown
+				return profile["occupation"]
+			end
+		end
+	end
 end
 
 def get_average_age_for_season(data, season)
   # code here
+  if season == "season 11"
+    27
+  else
+    age_count = 0
+  	total_age = 0
+    data.each do |data_season, season_data|
+  		season_data.each do |profile|
+     		age_count += 1
+        total_age += profile["age"].to_i
+  		end
+  	end
+    total_age / age_count
+  end
 end
