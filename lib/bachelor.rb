9e85b10c051ed9f00f0fc65ac6b5bae253205da0
Elias Miller <emiller783@gmail.com>
2016-07-30 19:30:46 +0000

    bachelor done

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,45 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data[season].each do |hash|
+    return hash["name"].split.first if hash["status"] == "Winner"
+  end
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |season, season_info|
+    season_info.each do |hash|
+      return hash["name"] if hash["occupation"] == occupation
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  count = 0
+  data.each do |season, season_info|
+    season_info.each do |hash|
+      count += 1 if hash["hometown"] == hometown
+    end
+  end
+  count
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |season, season_info|
+    season_info.each do |hash|
+      return hash["occupation"] if hash["hometown"] == hometown
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  total = 0
+  num = 0
+  data[season].each do |attribute|
+    attribute.each do |x, y|
+      if x == "age"
+        total += y.to_f
+      end
+    end
+    num += 1
+  end
+  return (total/num).to_f.round
 end
