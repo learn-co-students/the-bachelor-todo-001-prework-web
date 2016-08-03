395820f07105ce58daf7fdc8985249e1ffdcfca1
Steven Tang <5teven7ang@gmail.com>
2016-08-03 07:24:19 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,28 @@
+require 'pry'
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  winner_hash = data[season].select {|k| k["status"] == "Winner"}[0]
+  winner_hash["name"].split(' ')[0]
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  contestant_hash = data.collect {|k,v| v.select {|l,b| l["occupation"] == occupation}}.flatten.compact[0]
+  contestant_hash["name"]
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  hometown_hash = data.collect {|k,v| v.select {|l,b| l["hometown"] == hometown}}.flatten.compact.count
 end
 
 def get_occupation(data, hometown)
-  # code here
+  hometown_hash = data.collect {|k,v| v.select {|l,b| l["hometown"] == hometown}}.flatten.compact[0]
+  hometown_hash["occupation"]
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  age_array = data[season].collect {|k| k["age"]}
+  age_array = age_array.collect {|item| item.to_i}
+  age_sum = age_array.inject(0.0,:+)
+  age_size = age_array.length
+  age_avg = (age_sum/age_size).round
 end
